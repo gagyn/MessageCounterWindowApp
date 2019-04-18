9cad18bd91fe368918fa142a47dbeb590ecebd42
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-04-19 01:23:17 +0200

    Made possible to start with args (you can drop on the program a .json file)

--- MessageCounterBackend/StatContainers/ListTypesClasses/Person.cs
+++ MessageCounterBackend/StatContainers/ListTypesClasses/Person.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MessageCounterBackend.JsonStructure;
 
 namespace MessageCounterBackend.StatContainers.ListTypesClasses
 {
     class Person
     {
+        public Person(List<Message> messages, string personName)
+        {
+
+        }
     }
 }
