2e7d3a2c7074d5d3893b96225b6e009158e37002
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-04-18 20:17:36 +0200

    TextBoxMaker now is static. Added String makers for every container (don't make anything yet).

--- MessageCounterFrontend/InterfaceBackend/TextBoxMaker.cs
+++ MessageCounterFrontend/InterfaceBackend/TextBoxMaker.cs
@@ -1,32 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MessageCounterBackend;
+using MessageCounterBackend.StatContainers;
 using System.Windows.Controls;
 
 namespace MessageCounterFrontend.InterfaceBackend
 {
-    internal class TextBlockMaker
+    internal static class TextBlockMaker
     {
-        public bool IncludePeople { get; set; }
-        public bool IncludeDays { get; set; }
-        public bool IncludeMessages { get; set; }
-        public TextBlock GetTextBlock() => textBox;
+        public static bool IncludePeople { get; set; }
+        public static bool IncludeDays { get; set; }
+        public static bool IncludeMessages { get; set; }
 
-        private readonly TextBlock textBox;
-        public TextBlockMaker(MessageCounterBackend.StatsContainer statsContainer)
-        {
-            this.textBox = new TextBlock()
-            {
-                Text = PrepareStatsToString(statsContainer)
-            };
-        }
-
-        private string PrepareStatsToString(MessageCounterBackend.StatsContainer statsContainer)
+        public static TextBlock PrepareStatsToString(StatsContainer statsContainer)
         {
             string content;
             content = "Number of all message in this conversation: " + statsContainer.NumberOfMessages.ToString();
+
+            if (IncludeDays)
+                content += MakeDaysStatsString(statsContainer.daysContainer);
+
+            var textBlock = new TextBlock()
+            {
+                Text = content
+            };
+            return textBlock;
+        }
+
+        private static string MakePeopleString(PeopleContainer people)
+        {
+            string content;
+
+
+            return content;
+        }
+
+        private static string MakeDaysStatsString(DaysContainer days)
+        {
+            string content;
+            content = "The larger number of messages in single day: " + days.dayWithMaxNumberOfMessages.NumberOfMessages;
+            content += " on " + days.dayWithMaxNumberOfMessages.thisDateTime.ToShortDateString() + "\n";
+
+            return content;
+        }
+
+        private static string MakeMessagesStatsString(MessagesContainer messages)
+        {
+            string content;
 
 
             return content;
