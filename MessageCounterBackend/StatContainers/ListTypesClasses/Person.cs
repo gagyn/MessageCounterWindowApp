fc0f24466c94cc81b28ac6c3f93e74e902f1093c
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-04-19 01:23:49 +0200

    Revert "Made possible to start with args (you can drop on the program a .json file)"
    This reverts commit 9cad18bd91fe368918fa142a47dbeb590ecebd42.

--- MessageCounterBackend/StatContainers/ListTypesClasses/Person.cs
+++ MessageCounterBackend/StatContainers/ListTypesClasses/Person.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using MessageCounterBackend.JsonStructure;
 
 namespace MessageCounterBackend.StatContainers.ListTypesClasses
 {
     class Person
     {
-        public Person(List<Message> messages, string personName)
-        {
-
-        }
     }
 }
