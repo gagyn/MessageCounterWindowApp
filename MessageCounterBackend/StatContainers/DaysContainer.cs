efc40e0d0b23435335d91ac137cc6402e4ddea8f
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-04-18 16:58:57 +0200

    Changed name of folder and added new containers classes

---
+++ MessageCounterBackend/StatContainers/DaysContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageCounterBackend.StatClasses
+{
+    class DaysContainer
+    {
+        private List
+        public DaysContainer(JsonStructureClass jsonObject)
+        {
+
+        }
+    }
+}
---
+++ MessageCounterBackend/StatContainers/ListTypesClasses/Day.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageCounterBackend.StatContainers.ListTypesClasses
+{
+    class Day
+    {
+    }
+}
