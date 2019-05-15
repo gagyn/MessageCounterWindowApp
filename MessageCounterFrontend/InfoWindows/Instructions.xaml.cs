048120181e21c27245ec4ecc4e3feaff539f339f
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-05-15 17:52:08 +0200

    Instructions page prepared.

---
+++ MessageCounterFrontend/InfoWindows/Instructions.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace MessageCounterFrontend.InfoWindows
+{
+    /// <summary>
+    /// Interaction logic for Instructions.xaml
+    /// </summary>
+    public partial class Instructions : Page
+    {
+        public Instructions()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e) 
+            => System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+
+        private void ReturnButton_Click(object sender, System.Windows.RoutedEventArgs e)
+            => NavigationService.GoBack();
+    }
+}
